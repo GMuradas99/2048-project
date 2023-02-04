16e20bbab848b11bed13e5525dc4a2218d7adea1
Arthur Tavares <arthurtquintao@gmail.com>
2023-02-04 13:47:22 +0100

    bind 'r' key for resetting

--- Controller.py
+++ Controller.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from Model import Model
 from View import GameGrid
 
@@ -10,7 +12,19 @@ class Controller:
     def start(self):
         self.view.setup(self)
         self.view.update_grid_cells()
+
+        self.view.master.bind('<KeyPress>', self.key_pressed)
+
         self.view.start_main_loop()
+
+    def key_pressed(self, event):
+        if event.char == "r":
+            self.reset()
+
+    def reset(self):
+        self.view.destroy()
+        self.model = Model(4)  # todo: avoid hardcoded length
+        self.start()
 
 
 game = Controller(Model(4), GameGrid())
