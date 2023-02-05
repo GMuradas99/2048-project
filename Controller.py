711e448fb4caaee08a5a45ee54bd224841818fb3
Arthur Tavares <arthurtquintao@gmail.com>
2023-02-05 16:49:36 +0100

    add control for other movements

--- Controller.py
+++ Controller.py
@@ -1,5 +1,3 @@
-from time import sleep
-
 from Model import Model
 from View import GameGrid
 
@@ -7,6 +5,8 @@ from View import GameGrid
 class Controller:
     def __init__(self, model: object, view: object):
         """
+        Controller for 2048 game.
+        It manipulates the model according to the keys pressed and updates the view.
 
         :param model: game logic
         :param view: game UI
@@ -26,10 +26,34 @@ class Controller:
         if event.char == "r":
             self.reset()
 
+        elif event.char == "w":
+            self.move_up()
+
+        elif event.char == "a":
+            self.move_left()
+
+        elif event.char == "s":
+            self.move_down()
+
+        elif event.char == "d":
+            self.move_right()
+
     def reset(self):
         self.view.destroy()
         self.model = Model(self.model.size)
         self.start()
+
+    def move_up(self):
+        self.model.move_up()
+
+    def move_left(self):
+        self.model.move_left()
+
+    def move_down(self):
+        self.model.move_down()
+
+    def move_right(self):
+        self.model.move_rigth()
 
 
 game = Controller(Model(4), GameGrid())
